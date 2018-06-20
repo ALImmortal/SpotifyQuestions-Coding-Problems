17eb73352ddca63a933f54544a63f805bc57f5c2
Anne Liang <5193436+ALImmortal@users.noreply.github.com>
2018-06-20 10:59:44 -0400

    Update Question3.c

--- Question3.c
+++ Question3.c
@@ -11,3 +11,46 @@ Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your p
 1¢, 3¢
 2¢, 2¢
 **********************************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int changePossibilities(int amount, int *d){
+  int count=0;
+  int tempAmount=amount;//If not used, amount in the loop will be changed and cannot be reused
+  int dSize=sizeof(d) / sizeof(*d);
+  
+  for(int x=0;x<dSize;x++){
+    while(tempAmount!=0){
+      tempAmount-=d[x];
+      if(tempAmount>=d[x+1]){
+        tempAmount-=d[x+1];
+      }
+      else if(tempAmount<d[x+1]){
+        tempAmount-=d[x];
+      }
+    }
+    count++;
+    tempAmount=amount; //Reset the value
+  }
+
+  return count;
+}
+
+int main() {
+  int value;
+  int denomCount;
+  int posC=0;
+  cout<<"Enter the amount: ";
+  cin>>value;
+
+  cout<<"Enter the number of denominators to be used: ";
+  cin>>denomCount;
+  int denom[denomCount];
+  for(int i=0;i<denomCount;i++){
+    cout<<"Enter denominator: ";
+    cin>>denom[i];
+  }
+
+  cout<<"Count = "<<changePossibilities(value, denom)<<endl;
+}
